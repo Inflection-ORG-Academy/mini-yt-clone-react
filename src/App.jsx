6a6e5cda0d959792b9c1aa7f8dffbe911dbc4ba2
Deepak Maurya <deepak04122000maurya@gmail.com>
2025-11-07 08:12:00 +0530

    Project Layout created

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,25 @@ import { Route, Routes } from "react-router";
 
 import HomePage from "./pages/Home";
 import ErrorPage from "./ErrorPage";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
+    <div>
+      <Navbar />
+      <main className="flex">
+        <Sidebar />
 
-      <Route path="*" element={<ErrorPage />} />
-    </Routes>
+        <section className="min-h-screen">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+
+            <Route path="*" element={<ErrorPage />} />
+          </Routes>
+        </section>
+      </main>
+    </div>
   );
 }
 
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Sidebar() {
+  return (
+    <aside className="w-[200px] h-screen bg-neutral-800 text-neutral-100">
+      Side Bar
+    </aside>
+  );
+}
+
+export default Sidebar;
