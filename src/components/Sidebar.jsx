4ac293bb069ea4c1412c26c77390f38640f574fc
Deepak Maurya <deepak04122000maurya@gmail.com>
2025-11-07 08:39:42 +0530

    Sidebar with active state implemented

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,51 @@
 import React from "react";
+import { Link, NavLink } from "react-router";
+
+import { AiFillHome } from "react-icons/ai";
+import { SiYoutubeshorts } from "react-icons/si";
+import { MdSubscriptions } from "react-icons/md";
 
 function Sidebar() {
   return (
     <aside className="w-[200px] h-screen bg-neutral-800 text-neutral-100">
-      Side Bar
+      <ul>
+        <li>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive
+                ? "bg-neutral-200/10 flex gap-2 items-center p-2 text-sm"
+                : "bg-transparent flex gap-2 items-center p-2 text-sm"
+            }
+          >
+            <AiFillHome size={20} />
+            <span>Home</span>
+          </NavLink>
+          <NavLink
+            to="/shorts"
+            className={({ isActive }) =>
+              isActive
+                ? "bg-neutral-200/10 flex gap-2 items-center p-2 text-sm"
+                : "bg-transparent flex gap-2 items-center p-2 text-sm"
+            }
+          >
+            <SiYoutubeshorts size={20} />
+
+            <span>Shorts</span>
+          </NavLink>
+          <NavLink
+            to="/subcriptions"
+            className={({ isActive }) =>
+              isActive
+                ? "bg-neutral-200/10 flex gap-2 items-center p-2 text-sm border-b border-b-neutral-200/20"
+                : "bg-transparent flex gap-2 items-center p-2 text-sm border-b border-b-neutral-200/20"
+            }
+          >
+            <MdSubscriptions size={20} />
+            <span>Subscription</span>
+          </NavLink>
+        </li>
+      </ul>
     </aside>
   );
 }
